d8ac03552c9c1113fba62eb4551a193755893470
RojasLeonJuanLuis <rojasleon122@gmail.com>
2017-12-27 18:19:42 -0600

    search-bar ready

--- src/videos-youtube/components/loading.js
+++ src/videos-youtube/components/loading.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const ContainerLink = styled.div`
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
 
 const Loading = () => (
-  <div>
-    Loading...
-  </div>
+  <ContainerLink>
+
+    <h2 style={{fontSize: '48px'}}>Loading...</h2>
+  </ContainerLink>
 )
 export default Loading
--- src/videos-youtube/containers/search-bar.js
+++ src/videos-youtube/containers/search-bar.js
@@ -1,4 +1,38 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Search = styled.div`
+	display: flex;
+	justify-content: center;
+	padding: 15px;
+	box-shadow: 0 0 5px 10px #D9D9D9;
+	margin-bottom: 20px;
+	flex-wrap:wrap;
+`
+const Input = styled.input`
+	padding: 10px;
+	width: 30%;
+	border-radius: 5px;
+	border: 1px solid #ccc;
+	font-size: 18px;
+	@media(max-width: 820px) {
+		width: 90%;
+	}
+`
+const Button = styled.button`
+	display: none;
+	@media(max-width: 820px) {
+		display:inline-block;
+		width: 50%;
+		border-radius: 5px;
+		margin-left: 10px;
+		padding: 10px;
+		margin-top: 12px;
+		background-color: rgb(42,48,61);
+		color: #fff;
+		font-size: 18px;
+	}
+`
 
 export default class SearchBar extends Component {
 	state = { term: ''}
@@ -10,10 +44,14 @@ export default class SearchBar extends Component {
 
 	render() {
 		return (
-			<input
-				value={this.state.term}
-				onChange={event => this.handleSearch(event.target.value)}
-			/>
+			<Search>
+				<Input
+					placeholder="Search your favorite videos"
+					value={this.state.term}
+					onChange={event => this.handleSearch(event.target.value)}
+				/>
+				<Button>Search</Button>
+			</Search>
 		)
 	}
 }
